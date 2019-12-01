b4be7504f66d7c0c8ad2b6350456848180aea5ef
Gary Clark <gclark04432@live.co.uk>
2019-12-01 14:23:31 +0000

    add-celebrate-favourite-song-function

--- guest.rb
+++ guest.rb
@@ -1,11 +1,19 @@
 class Guest
 
-attr_reader :name, :age, :wallet
+  attr_reader :name, :age, :wallet, :favourite_song
 
-def initialize(name, age, wallet)
-  @name = name
-  @age = age
-  @wallet = wallet
-end
+  def initialize(name, age, wallet, favourite_song)
+    @name = name
+    @age = age
+    @wallet = wallet
+    @favourite_song = favourite_song
+  end
+
+  def celebrate_favourite_song(song)
+    if song.title() == @favourite_song
+      return "I love this song!!"
+    end
+    return false
+  end
 
 end
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -8,10 +8,15 @@ require_relative ('../song')
 class TestGuest < Minitest::Test
 
   def setup
-    @guest1 = Guest.new("Gary Clark", 33, 100)
-    @guest2 = Guest.new("Graeme Herd", 34, 50)
-    @guest3 = Guest.new("Darren Clark", 17, 100)
-    @guest4 = Guest.new("Stuart Martin", 18, 0)
+    @guest1 = Guest.new("Gary Clark", 33, 100, "The Kill")
+    @guest2 = Guest.new("Graeme Herd", 34, 50, "American Boy")
+    @guest3 = Guest.new("Darren Clark", 17, 100, "Here With Me")
+    @guest4 = Guest.new("Stuart Martin", 18, 0, "Rap Devil")
+
+    @song1 = Song.new("American Boy", "Estelle", 2008)
+    @song2 = Song.new("Rap Devil", "Machine Gun Kelly", 2018)
+    @song3 = Song.new("The Kill", "30 Seconds to Mars", 2005)
+    @song4 = Song.new("Here With Me", "Dido", 1999)
   end
 
   def  test_can_read_guest_name
@@ -26,5 +31,16 @@ class TestGuest < Minitest::Test
     assert_equal(100, @guest1.wallet())
   end
 
+  def test_can_read_favourite_song
+    assert_equal("Rap Devil", @guest4.favourite_song())
+  end
+
+  def test_celebrate_favourite_song_works
+    assert_equal("I love this song!!", @guest1.celebrate_favourite_song(@song3))
+  end
+
+  def test_celebrate_favourite_song_returns_false_if_not_correct
+    assert_equal(false, @guest2.celebrate_favourite_song(@song3))
+  end
 
 end
